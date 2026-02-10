cc2bb7f594324c2b1ab6b1e01dab59b08e8985ab
Seroga <ilchuk.cergey@gmail.com>
2026-02-10 19:45:46 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from typing import Any, Iterator
 
 
+_DELETED = object()
+
+
 class Dictionary:
     def __init__(self) -> None:
         self.length = 0
@@ -10,9 +13,9 @@ class Dictionary:
         old_hash_table = self.hash_table
         current_len = len(self.hash_table)
         self.length = 0
-        self.hash_table = ([None] * current_len) * 2
+        self.hash_table = [None] * (current_len * 2)
         for node in old_hash_table:
-            if node is None or node == "Deleted":
+            if node is None or node is _DELETED:
                 continue
             else:
                 key, hash_value, value = node
@@ -23,21 +26,39 @@ class Dictionary:
         if self.length + 1 >= capacity * 2 // 3:
             self.resize()
             capacity = len(self.hash_table)
+
         capacity_try = capacity
         hash_value = hash(key)
         index = hash_value % capacity
         node = (key, hash_value, value)
+
+        first_deleted: int | None = None
+
         while capacity_try != 0:
             if self.hash_table[index] is None:
-                self.hash_table[index] = node
+                if first_deleted is not None:
+                    self.hash_table[first_deleted] = node
+                else:
+                    self.hash_table[index] = node
                 self.length += 1
-                break
-            elif self.hash_table[index][0] == node[0]:
+                return
+
+            if self.hash_table[index] is _DELETED:
+                if first_deleted is None:
+                    first_deleted = index
+            elif self.hash_table[index][0] == key:
                 self.hash_table[index] = node
-                break
-            else:
-                index = (index + 1) % capacity
-                capacity_try -= 1
+                return
+
+            index = (index + 1) % capacity
+            capacity_try -= 1
+
+        if first_deleted is not None:
+            self.hash_table[first_deleted] = node
+            self.length += 1
+            return
+
+        raise RuntimeError("Hash table is full")
 
     def __getitem__(self, key: Any) -> Any:
         capacity = len(self.hash_table)
@@ -47,11 +68,14 @@ class Dictionary:
         while capacity_try != 0:
             if self.hash_table[index] is None:
                 raise KeyError(key)
-            elif self.hash_table[index][0] == key:
+
+            if (self.hash_table[index] is not _DELETED
+                    and self.hash_table[index][0] == key):
                 return self.hash_table[index][2]
-            else:
-                index = (index + 1) % capacity
-                capacity_try -= 1
+
+            index = (index + 1) % capacity
+            capacity_try -= 1
+
         raise KeyError(key)
 
     def __len__(self) -> int:
@@ -70,9 +94,9 @@ class Dictionary:
             if self.hash_table[index] is None:
                 raise KeyError(key)
             elif self.hash_table[index][0] == key:
-                self.hash_table[index] = "Deleted"
+                self.hash_table[index] = _DELETED
                 self.length -= 1
-                break
+                return
             else:
                 index = (index + 1) % capacity
                 capacity_try -= 1
@@ -86,11 +110,14 @@ class Dictionary:
         while capacity_try != 0:
             if self.hash_table[index] is None:
                 return default
-            elif self.hash_table[index][0] == key:
+
+            if (self.hash_table[index] is not _DELETED
+                    and self.hash_table[index][0] == key):
                 return self.hash_table[index][2]
-            else:
-                index = (index + 1) % capacity
-                capacity_try -= 1
+
+            index = (index + 1) % capacity
+            capacity_try -= 1
+
         return default
 
     def pop(self, key: Any, default: Any = None) -> Any:
@@ -100,14 +127,21 @@ class Dictionary:
         index = hash_value % capacity
         while capacity_try != 0:
             if self.hash_table[index] is None:
-                raise KeyError(key)
-            elif self.hash_table[index][0] == key:
+                if default is None:
+                    raise KeyError(key)
+                return default
+
+            if (self.hash_table[index] is not _DELETED
+                    and self.hash_table[index][0] == key):
                 result = self.hash_table[index][2]
                 self.__delitem__(key)
                 return result
-            else:
-                index = (index + 1) % capacity
-                capacity_try -= 1
+
+            index = (index + 1) % capacity
+            capacity_try -= 1
+
+        if default is None:
+            raise KeyError(key)
         return default
 
     def update(self, other: Any = None, **kwargs) -> None:
@@ -124,6 +158,6 @@ class Dictionary:
 
     def __iter__(self) -> Iterator[Any]:
         for cell in self.hash_table:
-            if cell is None or cell == "Deleted":
+            if cell is None or cell is _DELETED:
                 continue
             yield cell[0]
